fd25965e91795bd8debdea5901940f0f6c438897
AmjadxDev <AmjadxDev@gmail.com>
2025-02-20 15:10:27 +0500

    Update in background service start stop

--- app/src/main/java/com/example/servicesexample/BgService.kt
+++ app/src/main/java/com/example/servicesexample/BgService.kt
@@ -7,23 +7,35 @@ import android.util.Log
 import kotlin.concurrent.thread
 
 class BgService : Service() {
+    private var isServiceRunning = false
     override fun onBind(intent: Intent?): IBinder? = null
+
 
     override fun onCreate() {
         super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        thread(start = true) {
-            while (true) {
-                Log.d("BG_SERVICES", "BG Service Running!")
-                Thread.sleep(1000)
-            }
+
+        if (!isServiceRunning) {
+            isServiceRunning = true
+            startBackgroundTask()
         }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        isServiceRunning = false
         super.onDestroy()
+    }
+
+    fun startBackgroundTask() {
+        thread(start = true) {
+            while (isServiceRunning) {
+                Log.d("BG_SERVICES", "BG Service Running!")
+                Thread.sleep(1000)
+            }
+        }
     }
 }
